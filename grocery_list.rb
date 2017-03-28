0bea092291e8b62e47e9ebf5f118dd2ab2abfcae
Paraprax <parkerjohnston28@gmail.com>
2017-03-28 16:53:10 -0400

    updating grocery list program

--- grocery_list.rb
+++ grocery_list.rb
@@ -1,5 +1,18 @@
  grocery_list = ["cookies", "pasta", "cereal", "hummus", "chips", "eggs"]
 
- grocery_list.each do |item|
-   puts "*#{item}"
- end
+    grocery_list.each do |item|
+        puts "*#{item}"
+      end
+
+  puts "Forgot rice. Adding rice to list...."
+
+  grocery_list << "rice"
+
+    grocery_list.each do |item|
+        puts "*#{item}"
+      end
+
+  puts "This list is #{grocery_list.length} items long."
+
+ #check for bananas
+    grocery_list.include?("bananas")
