abb0c26af0ca34a9e2e9ddd9be1750f8ff9d2a28
Paraprax <parkerjohnston28@gmail.com>
2017-03-28 15:35:09 -0400

    creating text wrapper method

---
+++ exercise5.rb
@@ -0,0 +1,3 @@
+def fahrelsius(fahr)
+  "The temperature in Celsius is #{(fahr.to_i - 32) * 5/9} degrees"
+end
---
+++ wraptext_method.rb
@@ -0,0 +1,5 @@
+def wrap_text(input, wrapper, wropper, wripper)
+ puts "#{wrapper}#{wropper}#{wripper}#{input}#{wripper}#{wropper}#{wrapper}"
+end
+
+ wrap_text('Parker','----','====','####')
