262cd958b860bfdb920b7d8e302a26c9c204494e
Paraprax <parkerjohnston28@gmail.com>
2017-03-29 14:51:05 -0400

    adding fourth cohort and increasing sizes by 5%

--- exercise10.rb
+++ exercise10.rb
@@ -3,3 +3,27 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22,
 }
+
+  students.each do |k,v|
+    puts "#{k},#{v}"
+  end
+
+puts "adding cohort4"
+
+  students[:cohort4] = 43
+
+puts students.keys
+
+puts "Increasing cohort sizes by 5%"
+
+  students.each do |k,v|
+    puts "#{k},#{v*1.05}"
+  end
+
+  students.delete(:cohort2)
+
+  puts "deleting second cohort"
+
+  puts students.each do |k,v|
+      puts "#{k},#{v*1.05}"
+    end
